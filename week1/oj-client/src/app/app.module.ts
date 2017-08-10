6a8b4d9870529b55ff62bbb2a0744ed7dfa86be3
Doris <mingxinwb@gmail.com>
2017-08-10 16:17:01 -0700

    add mock db and data service, components can load data from data service, data service load mock db

--- week1/oj-client/src/app/app.module.ts
+++ week1/oj-client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DataService } from './services/data.service';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
 
 @NgModule({
@@ -12,7 +13,12 @@ import { ProblemListComponent } from './components/problem-list/problem-list.com
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'data',
+      useClass: DataService
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
