9da4b7c65948dc646518940219c2fa846f5a2750
Doris <mingxinwb@gmail.com>
2017-08-19 17:06:24 -0700

    create web server, add server.js, problemService, design web-side router

---
+++ week2/oj-server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const restRouter = require('./routes/rest');
+
+app.get('/', (req, res) => {
+  res.send('Hello Doris!')
+});
+
+app.use('/api/v1', restRouter);
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+});
