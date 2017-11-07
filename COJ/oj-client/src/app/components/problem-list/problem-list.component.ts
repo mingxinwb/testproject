89ba27c3c23f2a1d4a93bb2d2645bdcf7299721b
mingxinwb <mingxinwb@gmail.com>
2017-11-06 18:04:41 -0800

    generate problem-list component

---
+++ COJ/oj-client/src/app/components/problem-list/problem-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Problem } from '../../models/problem.model';
+import { PROBLEMS } from '../../mock-problems';
+
+@Component({
+  selector: 'app-problem-list',
+  templateUrl: './problem-list.component.html',
+  styleUrls: ['./problem-list.component.css'],
+})
+export class ProblemListComponent implements OnInit {
+  problems: Problem[];
+  constructor() { }
+
+  ngOnInit() {
+    this.getProblems();
+  }
+
+  getProblems(): void {
+    this.problems = PROBLEMS;
+  }
+}
