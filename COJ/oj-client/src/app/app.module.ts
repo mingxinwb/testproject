be1d2bc6ee2f54efc24426883d1b3c67b9258010
mingxinwb <mingxinwb@gmail.com>
2017-11-06 22:43:34 -0800

    generate new-problem component

--- COJ/oj-client/src/app/app.module.ts
+++ COJ/oj-client/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -6,16 +7,19 @@ import { DataService } from './services/data.service';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
 import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
 import { routing } from './app.routes';
+import { NewProblemComponent } from './components/new-problem/new-problem.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProblemListComponent,
-    ProblemDetailComponent
+    ProblemDetailComponent,
+    NewProblemComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     routing
   ],
   providers: [
--- COJ/oj-client/src/app/services/data.service.ts
+++ COJ/oj-client/src/app/services/data.service.ts
@@ -15,5 +15,10 @@ export class DataService {
 
   getProblem(id:number): Problem {
     return this.problems.find((problem) => problem.id === id);
+  };
+
+  addProblem(problem: Problem) {
+    problem.id = this.problems.length + 1;
+    this.problems.push(problem);
   }
 }
